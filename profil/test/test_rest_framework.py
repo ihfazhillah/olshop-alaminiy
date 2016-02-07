66cc2fe1bc659488a8f823ef53d073211724f79d
ihfazhillah <mihfazhillah@gmail.com>
2016-02-07 16:49:08 +0700

    Because the profil-api view is "hello" string, not serialized object, the assertion return Value Error, fix it.. ! 	modified:   profil/test/test_rest_framework.py

--- profil/test/test_rest_framework.py
+++ profil/test/test_rest_framework.py
@@ -5,12 +5,20 @@ from profil.models import Profil
 from profil.serializers import ProfilSerializer
 
 class ProfilDRFTest(APITestCase):
-    def test_can_retrieve_first_object_and_return_200_status_code(self):
+    def setUp(self):
         Profil.objects.create(nama="nama",
                               tagline="tagline",
-                              deskripsi="deskripsi")
+                              deskripsi="deskripsi",
+                              alamat="")
+    def test_can_retrieve_first_object_and_return_200_status_code(self):
         response = self.client.get(reverse('profil-api'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_retrieve_first_object_as_expected(self):
+        expected = {'nama':'nama', 'tagline':'tagline', 'deskripsi':'deskripsi',
+        'alamat':'alamat'}
+        response = self.client.get(reverse('profil-api'))
+        self.assertEqual(dict(response.data), expected)
 
 
 class ProfilSerializerClass(APITestCase):
